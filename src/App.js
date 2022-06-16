96b5bff63043ca5a3bea83f24e8d46ec2e1ec1f0
Shuvo-Roy <shuvoroysp@gmail.com>
2022-06-16 12:42:46 +0600

    Component tree

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import mainComponent from './component/mainComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <mainComponent/>
     </div>
   );
 }
