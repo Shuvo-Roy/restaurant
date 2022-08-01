4182fbde3e2f0295500e15078e23366e8c9f8c63
Shuvo Roy <shuvoroysp@gmail.com>
2022-08-01 21:22:18 +0600

    some issue fixed

--- src/component/body/Body.js
+++ src/component/body/Body.js
@@ -3,15 +3,17 @@ import Menu from "./Menu";
 import Home from "./Home";
 import Contact from "./Contact";
 import About from "./About";
-import { Route , Redirect} from "react-router-dom";
+import { Route , Redirect ,Switch} from "react-router-dom";
 const Body = () => {
     return (
         <div>
-        <Route path="/" exact component={Home}/>
-        <Route path="/menu" exact component={Menu}/>
-        <Route path="/contact" exact component={Contact}/>
-        <Route path="/about" exact component={About}/>
-        <Redirect from="/" to="/home"/>
+            <Switch>
+                <Route path="/home" exact component={Home}/>
+                <Route path="/menu" exact component={Menu}/>
+                <Route path="/contact" exact component={Contact}/>
+                <Route path="/about" exact component={About}/>
+                <Redirect from="/" to="/home"/>
+            </Switch>
         </div>
     );
 }
