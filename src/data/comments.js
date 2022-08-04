0565f0eaf648940798e4128291ddceda75aa1a57
Shuvo Roy <shuvoroysp@gmail.com>
2022-08-04 20:17:59 +0600

    separate dishes and comments

---
+++ src/data/comments.js
@@ -0,0 +1,131 @@
+ const COMMENTS=
+    [
+        {
+            dishId:0,
+            id:0,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:0,
+            id:1,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:0,
+            id:2,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:0,
+            id:3,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:1,
+            id:0,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:1,
+            id:1,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:1,
+            id:2,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:1,
+            id:3,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:2,
+            id:0,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:2,
+            id:1,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:2,
+            id:2,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:2,
+            id:3,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:3,
+            id:0,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:3,
+            id:1,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:3,
+            id:2,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+        {
+            dishId:3,
+            id:3,
+            rating:5,
+            comment:"overall good, i like this",
+            author:"shuvo",
+            date:"2021-10-16T17:57:28.556094Z"
+        },
+    ]  
--- src/data/dishes.js
+++ src/data/dishes.js
@@ -6,37 +6,9 @@
         category:'meal',
         label:'Hot',
         price:'99',
+        featured: true,
         description:'A hamburger is a food consisting of fillings —usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll',
-        comments:[
-            {
-                id:0,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:1,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:2,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:3,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-        ]
+        
     },
     {
         id:1,
@@ -45,37 +17,8 @@
         category:'meal',
         label:'Hot',
         price:'199',
+        featured: false ,
         description:'Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. ',
-        comments:[
-            {
-                id:0,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:1,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:2,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:3,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-        ]
     },
     {
         id:2,
@@ -84,37 +27,9 @@
         category:'meal',
         label:'Hot',
         price:'299',
+        featured: true,
         description:'Chocolate cake or chocolate gâteau is a cake flavored with melted chocolate, cocoa powder, or both.',
-        comments:[
-            {
-                id:0,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:1,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:2,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:3,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-        ]
+        
     },
     {
         id:3,
@@ -123,37 +38,9 @@
         category:'meal',
         label:'Hot',
         price:'99',
+        featured: false,
         description:'Noodles are a type of food made from unleavened dough which is rolled flat and cut, stretched or extruded, into long strips or strings.',
-        comments:[
-            {
-                id:0,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:1,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:2,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-            {
-                id:3,
-                rating:5,
-                comment:"overall good, i like this",
-                author:"shuvo",
-                date:"2021-10-16T17:57:28.556094Z"
-            },
-        ]
+        
     },
    ]
 
